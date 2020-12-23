32bdb467e64c600370aef719740999e5f3eba963
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-23 20:23:04 +0530

    Change

--- src/components/locationDisplay.jsx
+++ src/components/locationDisplay.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 export default function LoactionDisplay() {
   return (
     <>
-      <div>{useLocation().pathname}</div>
+      <div data-testid="location-display">{useLocation().pathname}</div>
     </>
   );
 }
