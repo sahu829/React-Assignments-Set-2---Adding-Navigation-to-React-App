a4c8ae687952b05840f05f53e58ecc6c750cb93e
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-23 18:51:05 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <button>btn</button>
+      </div>
+    );
+  }
 }
-
 
 export default App;
