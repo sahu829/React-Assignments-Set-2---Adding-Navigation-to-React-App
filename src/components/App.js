c602f9a1396db0e2acbb0b4f671eedf95c6931a4
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-23 19:53:42 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component, useState } from "react";
+import { Route } from "react-router-dom";
 import "../styles/App.css";
+import About from "./about";
+import Home from "./home";
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        {/* Do not remove the main div */}
-        <button>btn</button>
-      </div>
+      <>
+        <div id="main">{/* Do not remove the main div */}</div>
+
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </>
     );
   }
 }
